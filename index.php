2d1426382e675590555176a67f7b5b52c7b5111d
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-06 12:57:07 +0100

    if statement changed to switch

--- index.php
+++ index.php
@@ -13,24 +13,34 @@ $action = $action = $_GET['action'] ?? DEFAULT_ACTION;
 $view = new View();
 
 $viewParams = [];
-if ($action === 'create')
-{   
-    $page = 'create';
-    $created = false;
-    if(!empty($_POST))
-    {
-        $created = true;
-        $viewParams = [
-            'title' => $_POST['title'],
-            'description' => $_POST['description']
-        ];
-    }
-    $viewParams['created'] = $created;
-}
-else 
+
+switch($action)
 {
-    $page = 'list';
-    $viewParams['resultList'] = "wyświetlamy notatki";
+    case 'create':
+        $page = 'create';
+        $created = false;
+        if(!empty($_POST))
+        {
+            $created = true;
+            $viewParams = [
+                'title' => $_POST['title'],
+                'description' => $_POST['description']
+            ];
+        }
+        $viewParams['created'] = $created;
+        break;
+
+    case 'show':
+        $viewParams = [
+            'title' => 'Moja notatka',
+            'description' => 'Opis'
+        ];
+        break;
+        
+    default:
+        $page = 'list';
+        $viewParams['resultList'] = "wyświetlamy notatki";
+        break;
 }
 
 $view->render($page, $viewParams);
