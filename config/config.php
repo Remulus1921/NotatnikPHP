8897bbed6edff48659eb246b0ccad0a7c13a8773
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-08 16:36:34 +0100

    DB configuration added

--- config/config.php
+++ config/config.php
@@ -1,8 +1,14 @@
 <?php
 
+declare(strict_types = 1);
+
 $configuration = [
+    'host' => 'notes.localhost',
+    'database' => 'notes',
+    'user' => 'user_notes',
     'password' => 'Om]m@*5vP(FBs3b1',
-]
+];
+
 
 
 ?>
