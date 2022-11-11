570e1551fe0e5acedc89e82adc7a22d5501a5ff6
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-11 13:43:34 +0100

    construcitn PDO to datbase added

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -4,10 +4,27 @@ declare(strict_types = 1);
 
 namespace App;
 
+use Exception;
+use PDO;
+
 class Database
 {
     public function __construct(array $config)
-    {
+    {   
+        $dsn = "mysql:database={$config['database']};host={$config['host']}";
+        try{
+            $connection = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password']
+            );
+            
+        } catch (Exception $e) {
+            dump($e);
+            exit;
+        }
+        dump($connection);
+
     }
 }
 
