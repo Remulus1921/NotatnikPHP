a01e165088c32667195e2ea0ea615953ae7d1201
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-12 16:36:52 +0100

    Try catch block and validateConfig function added

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -4,27 +4,45 @@ declare(strict_types = 1);
 
 namespace App;
 
-use Exception;
+require_once("src/Exception/StorageException.php");
+
+use App\Exception\ConfigurationException;
+use App\Exception\StorageException;
 use PDO;
+use PDOException;
+use Throwable;
 
 class Database
 {
     public function __construct(array $config)
     {   
-        $dsn = "mysql:database={$config['database']};host={$config['host']}";
-        try{
+        try
+        {
+            $this->validateConfig($config);
+            $dsn = "mysql:database={$config['database']};host={$config['host']}";
+
             $connection = new PDO(
                 $dsn,
                 $config['user'],
                 $config['password']
             );
-            
-        } catch (Exception $e) {
-            dump($e);
-            exit;
+        //dump($connection);
         }
-        dump($connection);
-
+        catch(PDOException $e)
+        {
+            throw new StorageException('connection error');
+        }
+    }
+    private function validateConfig(array $config): void
+    {
+        if(
+            empty($config['database']) 
+            || empty($config['host'])
+            || empty($config['user'])
+            || empty($config['password']))
+        {
+            throw new ConfigurationException('Stprage configuration error');
+        }
     }
 }
 
