ae8ede2447c45c4f5be5443071bab2a39934f00b
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-12 16:33:56 +0100

    Exception handling added

--- index.php
+++ index.php
@@ -3,8 +3,13 @@ declare(strict_types = 1);
 
 namespace App;
 
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
 require_once("src/Utils/Debug.php");
 require_once("src/Utils/Controller.php");
+require_once("src/Exception/AppException.php");
 
 $configuration = require_once("config/config.php"); 
 
@@ -13,11 +18,29 @@ $request = [
     'post' => $_POST
 ];
 
-//$controller = new Controller($request);
-//$controller->run();
 
-Controller::initConfiguration($configuration);
+try 
+{
+    //$controller = new Controller($request);
+    //$controller->run();
+    Controller::initConfiguration($configuration);
 
-(new Controller($request))->run(); //Robi to samo co dwie zakomentowane linijki powyżej w jednej lini
-
+    (new Controller($request))->run(); //Robi to samo co dwie zakomentowane linijki powyżej w jednej lini
+}
+catch(ConfigurationException $e)
+{
+    //mail('xxx@xxx.com', 'Error', $e->getMessage());
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo 'Problem z aplikacją. Proszę spróbować za chwilę.';
+}
+catch(AppException $e)
+{
+    echo "<h1>wystąpoł błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+}
+catch(Throwable $e)
+{
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    dump($e);
+}
 ?>
