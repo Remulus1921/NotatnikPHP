b53abe29cea975af3acbd936c3a9bd472c524651
Remigiusz Bekierz <remobekierz@onet.pl>
2022-11-13 12:27:53 +0100

    PDO exception heandler added and new query created

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -14,25 +14,55 @@ use Throwable;
 
 class Database
 {
+    private PDO $conn;
+
     public function __construct(array $config)
     {   
         try
         {
             $this->validateConfig($config);
-            $dsn = "mysql:database={$config['database']};host={$config['host']}";
-
-            $connection = new PDO(
-                $dsn,
-                $config['user'],
-                $config['password']
-            );
-        //dump($connection);
+            $this->createConnection($config);
         }
         catch(PDOException $e)
         {
             throw new StorageException('connection error');
         }
     }
+
+    public function createNote(array $data): void
+    {
+        try
+        {
+            $title = $this->conn->quote($data['title']);
+            $description = $this->conn->quote($data['description']);
+            $created = $this->conn->quote(date('Y-m-d H:i:s'));
+
+            $query = "INSERT INTO notes.notes(title, description, created) VALUES($title, $description, $created)";
+
+            $this->conn->exec($query);
+
+        }
+        catch(Throwable $e)
+        {
+            throw new StorageException('Nie udało się utworzyć nowej notatki', 400, $e);
+            exit();
+        }
+    }
+
+    private function createConnection(array $config):void
+    {
+        $dsn = "mysql:database={$config['database']};host={$config['host']}";
+
+            $this->conn = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+    }
+
     private function validateConfig(array $config): void
     {
         if(
